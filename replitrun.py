93c4a5a1541deb997db33d90440cabe65b35bb9f
Jason Tolbert <jasonalantolbert@gmail.com>
2020-11-23 13:44:17 -0500

    add replitrun.py

---
+++ replitrun.py
@@ -0,0 +1,4 @@
+print("Hello! This repl exists solely to demonstrate the functionality of the 'Run on Repl.it' badge\n"
+      "and is not built to actually deploy the Repl.it Badger website. \n"
+      "\n"
+      "To see the website, head to [placeholder].")
